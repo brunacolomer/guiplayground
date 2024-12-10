03bdf4150e12445d7c9607f11eff71806b0125d9
Bruna <bruna.colomer@estudiantat.upc.edu>
2024-12-10 23:11:01 +0100

    plot jano csv

---
+++ build/CMakeFiles/ImGuiExample.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bruna/GameDev/guiplayground/src/GUIManager.cpp" "CMakeFiles/ImGuiExample.dir/src/GUIManager.cpp.o" "gcc" "CMakeFiles/ImGuiExample.dir/src/GUIManager.cpp.o.d"
+  "/home/bruna/GameDev/guiplayground/src/MenuSystem.cpp" "CMakeFiles/ImGuiExample.dir/src/MenuSystem.cpp.o" "gcc" "CMakeFiles/ImGuiExample.dir/src/MenuSystem.cpp.o.d"
+  "/home/bruna/GameDev/guiplayground/src/PlotSystem.cpp" "CMakeFiles/ImGuiExample.dir/src/PlotSystem.cpp.o" "gcc" "CMakeFiles/ImGuiExample.dir/src/PlotSystem.cpp.o.d"
+  "/home/bruna/GameDev/guiplayground/src/main.cpp" "CMakeFiles/ImGuiExample.dir/src/main.cpp.o" "gcc" "CMakeFiles/ImGuiExample.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bruna/GameDev/guiplayground/build/CMakeFiles/imgui.dir/DependInfo.cmake"
+  "/home/bruna/GameDev/guiplayground/build/CMakeFiles/implot.dir/DependInfo.cmake"
+  "/home/bruna/GameDev/guiplayground/build/third_party/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/bruna/GameDev/guiplayground/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ImGuiExample.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImGuiExample.dir/src/GUIManager.cpp.o"
+  "CMakeFiles/ImGuiExample.dir/src/GUIManager.cpp.o.d"
+  "CMakeFiles/ImGuiExample.dir/src/MenuSystem.cpp.o"
+  "CMakeFiles/ImGuiExample.dir/src/MenuSystem.cpp.o.d"
+  "CMakeFiles/ImGuiExample.dir/src/PlotSystem.cpp.o"
+  "CMakeFiles/ImGuiExample.dir/src/PlotSystem.cpp.o.d"
+  "CMakeFiles/ImGuiExample.dir/src/main.cpp.o"
+  "CMakeFiles/ImGuiExample.dir/src/main.cpp.o.d"
+  "ImGuiExample"
+  "ImGuiExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImGuiExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
